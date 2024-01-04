706b83f1e65aa1dc3aeeb3eed137fdcbeab37af6
Luke Song <hyeonminsong7@gmail.com>
2024-01-04 15:11:06 -0800

    :adhesive-bandage: .then to async & await

--- script.js
+++ script.js
@@ -8,15 +8,26 @@ dice.addEventListener("click", () => {
   showQuote();
 });
 
-function showQuote() {
-  fetch("https://api.adviceslip.com/advice")
-    .then((response) => response.json())
-    .then((data) => data.slip)
-    .then((data) => {
-      adviceNum.textContent = data.id;
-      adviceText.textContent = data.advice;
-    })
-    .catch((error) => {
-      alert(`Error ${error}`);
-    });
+// function showQuote() {
+//   fetch("https://api.adviceslip.com/advice")
+//     .then((response) => response.json())
+//     .then((data) => data.slip)
+//     .then((data) => {
+//       adviceNum.textContent = data.id;
+//       adviceText.textContent = data.advice;
+//     })
+//     .catch((error) => {
+//       alert(`Error ${error}`);
+//     });
+// }
+
+async function showQuote() {
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    adviceNum.textContent = data.slip.id;
+    adviceText.textContent = data.slip.advice;
+  } catch (error) {
+    alert(`Error ${error}`);
+  }
 }
